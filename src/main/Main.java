4ac4a17456afa7460b428e39299cbebef1aa5548
Luis Martins <luishenrique.martinslima@gmail.com>
2025-12-11 10:13:06 -0300

    feat(Main): intancia FileRepository no Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,15 @@
 package main;
 
+import repository.FileRepository;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
+
+        FileRepository file = new FileRepository();
+        file.createFile();
+
     }
 }
